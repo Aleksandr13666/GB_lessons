1ca1e62cd88bbd90f3d66ffae2af55ada53e2309
Aleksandr Ad <aleksandr-ad@narod.ru>
2023-01-17 18:07:03 +0700

    Added square of number

--- Task00/Program.cs
+++ Task00/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 0. Демонстрация решения 
+// Напишите программу, которая  
+// 1. на вход принимает число 
+// 2. выдает его квадрат (число умноденное на себя). 
+// 3. вывод результата 
+ 
+// Например: 
+// 4 -> 16 
+// -3 -> 9 
+// -7 -> 49
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int square = number * number;
+Console.WriteLine($"Квадрат числа {number} равен = {square}");
