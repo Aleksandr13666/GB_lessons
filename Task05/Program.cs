1ba049b091c319e749da49ae630845afc4aefb3e
Aleksandr Ad <aleksandr-ad@narod.ru>
2023-01-17 21:06:30 +0700

    Разобрались с заданием №5

--- Task05/Program.cs
+++ Task05/Program.cs
@@ -4,3 +4,11 @@
 // 4 -> "-4, -3, -2, -1, 0, 1, 2, 3, 4" 
 // 2 -> " -2, -1, 0, 1, 2"
 
+Console.Write("Input a number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int count = -num;
+while (count<=num)
+{
+Console.Write(count + ",");
+count++;
+};
