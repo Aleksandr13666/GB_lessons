ec43526efe1605861eb08da9c70ed0a169ca7724
Aleksandr Ad <aleksandr-ad@narod.ru>
2023-01-18 22:50:29 +0700

    Added Task 9

---
+++ Task09/Program.cs
@@ -0,0 +1,23 @@
+﻿int number = new Random().Next(10,100);
+Console.WriteLine($"Случайное число из отрезка 10-99 {number}");
+
+// int firstDigit = number / 10;
+// int secondDigit = number % 10;
+
+// // if (firstDigit > secondDigit)
+// // Console.WriteLine($"Наибольшая цифра числа -> {firstDigit}");
+// // else
+// // Console.WriteLine($"Наибольшая цифра числа ->  {secondDigit}");
+
+// int result = firstDigit > secondDigit ? firstDigit: secondDigit;
+// Console.WriteLine($"Наибольшая цифра числа -> {result}");
+
+int maxDigit = MaxDigit(number);
+Console.WriteLine($"Наибольшая цифра числа -> {maxDigit}");
+int MaxDigit(int num)
+{
+    int firstDigit = num / 10;
+    int secondDigit = num % 10;
+    int result = firstDigit > secondDigit ? firstDigit: secondDigit;
+    return result;
+}
