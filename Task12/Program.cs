dfc5c2233965c3b7ed64bf5d4de11a0abbddcf86
Aleksandr Ad <aleksandr-ad@narod.ru>
2023-01-21 16:40:17 +0700

    Complete Task 12

---
+++ Task12/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Input digit: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input divider: ");
+int divider = Convert.ToInt32(Console.ReadLine());
+
+void Multiple(int num, int div)
+{
+    if(num % div == 0)
+    {
+        Console.WriteLine($"{number} кратно {divider}");
+    }
+    else Console.WriteLine($"{number} при делении на {divider} имеет остаток = {num % div}");
+};
+
+Multiple(number, divider);
