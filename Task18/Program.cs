af0bbe48aebc6120e910fef584a021b0151b4db3
Aleksandr Ad <aleksandr-ad@narod.ru>
2023-01-21 23:57:44 +0700

    added Task18

---
+++ Task18/Program.cs
@@ -0,0 +1,20 @@
+﻿// 18. Напишите программу, которая по заданному номеру четверти показывает
+// возможные координаты точки (Х и У).
+
+
+
+Console.WriteLine("Input number of quarter: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+string result = Coordinate(number);
+
+Console.WriteLine(result);
+
+string Coordinate(int num)
+{
+    if (num == 1) return ($"Координаты соответствуют значениям Х и У -> x > 0 , y > 0");
+    if (num == 2) return ($"Координаты соответствуют значениям Х и У -> x < 0 , y > 0");
+    if (num == 3) return ($"Координаты соответствуют значениям Х и У -> x < 0 , y < 0");
+    if (num == 4) return ($"Координаты соответствуют значениям Х и У -> x > 0 , y < 0");
+    return ("Введены не корректные координаты");
+}
