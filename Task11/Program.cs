e98b06186dca048e12d0f50a0e3a927502b0860c
Aleksandr Ad <aleksandr-ad@narod.ru>
2023-01-21 16:12:45 +0700

    fixed name in Task11

--- Task11/Program.cs
+++ Task11/Program.cs
@@ -1,1 +1,24 @@
-﻿
+﻿// Напишите программу которая выводит 
+// случайное трехзначное число и 
+// удаляет втору цифру этого числа.
+// 456 -> 46
+// 782 -> 72
+// 918 -> 98
+
+
+
+int number = new Random().Next(100, 1000);
+Console.WriteLine($"Number is: {number}");
+
+int result = DelSecondaryNum(number);
+
+Console.WriteLine($"Result: {result}");
+
+int DelSecondaryNum(int num)
+{
+    int agr1 = num / 100;
+    int arg2 = num % 10;
+    int result = agr1 * 10 + arg2;
+    return result;
+}
+
