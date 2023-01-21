1982303e23d0820fad2d8c2b0bd060748e578fe1
Aleksandr Ad <aleksandr-ad@narod.ru>
2023-01-21 22:03:21 +0700

    Complete Task13

---
+++ Task13/Program.cs
@@ -0,0 +1,27 @@
+﻿// 13. Напишите программу, которая выводит 
+// третью цифру заданного числа или сообщает,
+// что третьей цифры нет.
+// 645 - 5
+// 78 - нет
+// 32679 - 6
+
+Console.WriteLine("Input your number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int thirdDig = ThirdDigit(number);
+
+if (number < 100)
+Console.WriteLine("Wrong input. Insert 3 digits number or more");
+else
+{
+    Console.WriteLine(thirdDig);
+}
+
+int ThirdDigit(int num)
+{
+    while (num >= 1000) 
+    // num = num / 10;
+    num /=10;
+    int res = num % 10;
+    return res;
+}
