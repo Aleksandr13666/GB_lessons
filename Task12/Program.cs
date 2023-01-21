a5dc988ae9bd51b8ed933b9e5967b603da8aae9e
Aleksandr Ad <aleksandr-ad@narod.ru>
2023-01-21 17:18:14 +0700

    Correcting Task12

--- Task12/Program.cs
+++ Task12/Program.cs
@@ -3,13 +3,27 @@ int number = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Input divider: ");
 int divider = Convert.ToInt32(Console.ReadLine());
 
-void Multiple(int num, int div)
-{
-    if(num % div == 0)
-    {
-        Console.WriteLine($"{number} кратно {divider}");
-    }
-    else Console.WriteLine($"{number} при делении на {divider} имеет остаток = {num % div}");
-};
+// void Multiple(int num, int div)
+// {
+//     if(num % div == 0)
+//     {
+//         Console.WriteLine($"{number} кратно {divider}");
+//     }
+//     else Console.WriteLine($"{number} при делении на {divider} имеет остаток = {num % div}");
+// };
 
-Multiple(number, divider);
+// Multiple(number, divider);
+int remains = Remains(number, divider);
+
+int Remains(int number, int divider)
+{
+    return number % divider;
+    
+}
+if(remains == 0)
+{
+    Console.WriteLine("Нет остатка.");
+}
+else
+Console.WriteLine($"остаток {remains}");
+
