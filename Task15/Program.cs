9825a961c9ba7c0a6ca3a2bb5b492b7761c1b388
Aleksandr Ad <aleksandr-ad@narod.ru>
2023-01-21 22:16:29 +0700

    Complete Task 15

---
+++ Task15/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("Input digit of a week: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+switch(num)
+{
+    case 1: Console.WriteLine("Is a Monday. Not a weekend");
+    break;
+    case 2: Console.WriteLine("Is a Tuesday. Not a weekend");
+    break;
+    case 3: Console.WriteLine("Is a Wednesday. Not a weekend");
+    break;
+    case 4: Console.WriteLine("Is a Thursday. Not a weekend");
+    break;
+    case 5: Console.WriteLine("Is a Friday. Not a weekend");
+    break;
+    case 6: Console.WriteLine("Is a Saturday. It's a weekend");
+    break;
+    case 7: Console.WriteLine("Is a Sunday. It's a weekend");
+    break;
+    default: Console.WriteLine("Wrong input");
+    break;
+}
+
+Console.WriteLine(num);
