941809183874b7ede8415acf1a67e0b74c2ddd9f
Aleksandr Ad <aleksandr-ad@narod.ru>
2023-01-22 14:49:28 +0700

    Format output string

--- Task22/Program.cs
+++ Task22/Program.cs
@@ -7,7 +7,7 @@ void Square(int num)
    int count = 1;
    while (count <= num)
    {
-     Console.WriteLine($"square of {count} is {count * count}");
+     Console.WriteLine($"square of {count, 4} is {count * count, 2}");
      count++;
    }
 
