8767454ba9e85c99906bc4fdfc9768c23769a05e
Aleksandr Ad <aleksandr-ad@narod.ru>
2023-01-22 14:45:37 +0700

    Added Task22

---
+++ Task22/Program.cs
@@ -0,0 +1,15 @@
+﻿
+Console.WriteLine("Input a number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+void Square(int num)
+{
+   int count = 1;
+   while (count <= num)
+   {
+     Console.WriteLine($"square of {count} is {count * count}");
+     count++;
+   }
+
+}
+Square(number);
