344ca3352d60d84615e3b8001739dd164fbbd750
Aleksandr Ad <aleksandr-ad@narod.ru>
2023-01-22 14:54:03 +0700

    Fixed Task22

--- Task22/Program.cs
+++ Task22/Program.cs
@@ -1,4 +1,8 @@
-﻿
+﻿// 22. Напишите программу которая на вход принимает число
+// N и выдаёт таблицу квадратов от 1 до числа N.
+// 5 -> 1, 4, 9 ,16 ,25
+
+
 Console.WriteLine("Input a number: ");
 int number = Convert.ToInt32(Console.ReadLine());
 
