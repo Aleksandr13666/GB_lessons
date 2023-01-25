612ea98b32df924c6fba14ab2b5c4b93dad0dfd9
Aleksandr Ad <aleksandr-ad@narod.ru>
2023-01-25 15:53:04 +0700

    Complete Task 23

--- Task23/Program.cs
+++ Task23/Program.cs
@@ -2,3 +2,18 @@
 // таблицу кубов чисел от 1 до введенного числа.
 // 3- 1,8,27
 // 5- 1,8,27,64
+
+Console.WriteLine("Input a number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+void Cube(int num)
+{
+   int count = 1;
+   while (count <= num)
+   {
+     Console.WriteLine($"Cube of {count, 4} is {count * count * count, 2}");
+     count++;
+   }
+
+}
+Cube(number);
