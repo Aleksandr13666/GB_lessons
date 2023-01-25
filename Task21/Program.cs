258e07471baad58aab1d9bb6d3b5b9aa73806a83
Aleksandr Ad <aleksandr-ad@narod.ru>
2023-01-25 16:43:54 +0700

    Complete Task21

--- Task21/Program.cs
+++ Task21/Program.cs
@@ -3,3 +3,26 @@
 // ними в 3-мерном пространстве.
 // А(3,6,8); В (2,1,-7) -> 15,84
 
+Console.WriteLine("Input X coordinate of dot A: ");
+int x1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input Y coordinate of dot A: ");
+int y1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input Y coordinate of dot A: ");
+int z1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input X coordinate of dot B: ");
+int x2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input X coordinate of dot B: ");
+int y2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input Y coordinate of dot B: ");
+int z2 = Convert.ToInt32(Console.ReadLine());
+
+double hypotenuse = Hypotenuse(x1, y1, z1, x2, y2, z2);
+
+Console.WriteLine($"Hypotenuse of two dots, A & B = {hypotenuse}");
+
+double Hypotenuse(int x1, int y1, int z1, int x2, int y2, int z2)
+{
+     double result = Math.Sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1) + (z2 - z1) * (z2 - z1));
+     double res = Math.Round(result, 2, MidpointRounding.ToZero);
+     return res;
+}
