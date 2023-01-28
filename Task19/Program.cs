15276a6872f2b97168f8340e37905f7630393182
Aleksandr Ad <aleksandr-ad@narod.ru>
2023-01-28 12:03:25 +0700

    Complete Task19

--- Task19/Program.cs
+++ Task19/Program.cs
@@ -3,3 +3,20 @@
 // палиндромом.
 // 14212 - Нет 
 // 23432 - Да
+
+
+Console.WriteLine("Input a five digit number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+bool Polyndrome(int num)
+{
+   int n = 0;
+   while( n < num)
+   {
+    n = 10 * n + num % 10;
+    num /= 10;
+   }
+return num == n || num == n / 10 ;
+}
+
+Console.WriteLine(Polyndrome(number));
