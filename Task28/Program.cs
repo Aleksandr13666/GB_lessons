983a2b1064809fbc991bd2fbe9ad4b86da64598e
Aleksandr Ad <aleksandr-ad@narod.ru>
2023-01-29 14:34:15 +0700

    Added Task28

---
+++ Task28/Program.cs
@@ -0,0 +1,28 @@
+﻿// 28. Напишите программу, которая принимает на вход число (N)
+// и выдаёт произведение чисел от 1 до N.
+// 5 - 120
+// 4 - 24
+
+
+
+Console.WriteLine("Input a number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int mult = Mult(number);
+
+int Mult(int number)
+
+{
+   
+   int count = 1;
+   int i = 1 ;
+   while(count <= number)
+   {
+       i *= count;
+       count++;
+   }
+   return i;
+
+}
+
+Console.WriteLine($"Sum all number is {mult}");
