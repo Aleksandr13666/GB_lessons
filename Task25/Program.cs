cb3ccc5d21de02500cfb4a3ce6a16b2c78375611
Aleksandr Ad <aleksandr-ad@narod.ru>
2023-01-30 19:41:00 +0700

    added task25

---
+++ Task25/Program.cs
@@ -0,0 +1,14 @@
+﻿
+
+Console.WriteLine("Input a number: ");
+int numA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input degree of number: ");
+int numB = Convert.ToInt32(Console.ReadLine());
+
+
+void Degree(int numA , int numB)
+{
+
+    
+}
+
