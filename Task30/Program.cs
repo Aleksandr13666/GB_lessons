ddf7323e7d1967b47ce739e42fa1800a58cd80db
Aleksandr Ad <aleksandr-ad@narod.ru>
2023-01-30 18:46:12 +0700

    Added Task30

--- Task30/Program.cs
+++ Task30/Program.cs
@@ -2,3 +2,29 @@
 // заполненый нулями и единицами в случайном порядке.
 // [1,0,0,0,1,1,1,0]
 
+
+void FillArr(int [] numbers)
+{
+    int length = numbers.Length;
+    int index = 0;
+    while(index < length)
+    {
+        numbers[index] = new Random().Next(0, 2);
+        index++;
+    }
+}
+ void PrintArr(int [] num)
+ {
+    int count = num.Length;
+    int nums = 0;
+    while(nums < count)
+    {
+        Console.WriteLine(num[nums]);
+        nums++;
+    }
+ }
+
+int [] array = new int[8];
+
+FillArr(array);
+PrintArr(array);
