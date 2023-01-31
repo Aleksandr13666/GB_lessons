6ff278f66e09f6bed7ab28374fbc942e4b7baf70
Aleksandr Ad <aleksandr-ad@narod.ru>
2023-02-01 01:06:12 +0700

    Added Task40

--- Task39/Program.cs
+++ Task39/Program.cs
@@ -1,1 +1,41 @@
-﻿
+﻿int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+    return arr;
+}
+
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write($"{arr[i]}, ");
+        else Console.Write($"{arr[i]}");
+    }
+    Console.Write("]");
+}
+
+void ReverseArray(int[] array)
+{
+    for (int i = 0; i < array.Length / 2; i++)//2
+    {
+        int temp = array[i]; //2
+        array[i] = array[array.Length - 1 - i];
+        array[array.Length - 1 - i] = temp;
+    }
+}
+
+int[] arr = CreateArrayRndInt(5, 1, 9);
+PrintArray(arr);
+ReverseArray(arr);
+Console.WriteLine();
+PrintArray(arr);
+
+Array.Reverse(arr);
+Console.WriteLine();
+PrintArray(arr);
---
+++ Task40/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 40: Напишите программу, которая принимает на вход три
+// числа и проверяет, может ли существовать треугольник с сторонами
+// такой длины.
+// Теорема о неравенстве треугольника: каждая сторона треугольника
+// меньше суммы двух других сторон.
+
+
+
+
+Console.WriteLine("Input first number ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input second number ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input third number ");
+int number3 = Convert.ToInt32(Console.ReadLine());
+
+bool Treangle(int num1, int num2, int num3)
+{
+    return num1 + num2 > num3 || num2 + num3 > num1 || num1 + num3 > num2;
+}
+bool tre = Treangle(number1, number2, number3);
+Console.WriteLine(tre ? "yes":"no");
