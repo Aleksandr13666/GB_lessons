f05488070c2ace1eacbe50a4bdddda6636645c34
Aleksandr Ad <aleksandr-ad@narod.ru>
2023-02-01 01:47:53 +0700

    Rebuild42

---
+++ Task42/Program.cs
@@ -0,0 +1,25 @@
+﻿// : Напишите программу, которая будет преобразовывать
+// десятичное число в двоичное.
+// 45 -> 101101
+// 3 -> 11
+// 2 -> 10
+
+
+
+Console.WriteLine("Input a digit: ");
+int digit = Convert.ToInt32(Console.ReadLine());
+
+int binary = Binary(digit);
+
+int Binary(int dig)
+{
+    int result = 0;
+    for(int i = 0; i < dig; i++)
+    {
+        result += dig % 2;
+        dig /= 2; 
+    }
+    return result;
+}
+
+Console.WriteLine(binary);
