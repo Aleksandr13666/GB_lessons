010e3085ac0bbe2cff5b232460ecbabdb1963ce2
Aleksandr Ad <aleksandr-ad@narod.ru>
2023-02-03 08:40:29 +0700

    Start create 44

---
+++ Task44/Program.cs
@@ -0,0 +1,16 @@
+﻿// FIB
+using Internal;
+
+Console.WriteLine("input a digit: ");
+ulong number = Convert.ToUInt64(Console.ReadLine());
+
+
+ulong Fib(ulong num)
+{
+    ulong n;
+    ulong result = 0;
+    for (int i = 1; i < num; i++)
+    {
+        
+    }
+}
