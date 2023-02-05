d4b623960304e6fa644d39a55bf8d9627158e452
Aleksandr Ad <aleksandr-ad@narod.ru>
2023-02-05 23:04:14 +0700

    38

--- Task38/Program.cs
+++ Task38/Program.cs
@@ -3,23 +3,26 @@
 // [3.5, 7.1, 22.9, 2.3, 78.5] -> 76.2
 
 
-double[] arr = {3.5, 7.1, 22.9, 2.3, 78.5};
 
-double diff = Diff(arr);
+double[] arr = {3.5, 7.1, 22.9, 2.3, 78.5};
+PrintArray(arr);
+Console.WriteLine("");
+double[] diff = Diff(arr);
 PrintArray(diff);
+Console.WriteLine($"{diff[0]}");
+
 double[] Diff(double[] array)
 {
-    double biggest = 0;
-    double small = 0;
-    int index = 0;
+    double max = 0;
+    double min = 0;
+    double difference = max - min;
     for (int i = 0; i < array.Length; i++)
     {
-        if(array[i] > biggest) biggest = array[i];
-        else if(array[i] < small) small = array[i];
-        index++;
+        if(array[i] > max) max = array[i];
+        else if(array[i] < min) min = array[i];
     }
-    double difference = biggest - small;
-    return difference;
+    
+    return new double[]{difference};
     
 }
 
