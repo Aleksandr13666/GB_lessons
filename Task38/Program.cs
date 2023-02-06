50916e38763e853971b576636f0586069c45102d
Aleksandr Ad <aleksandr-ad@narod.ru>
2023-02-06 22:08:55 +0700

    Complete Task38

--- Task38/Program.cs
+++ Task38/Program.cs
@@ -4,26 +4,30 @@
 
 
 
-double[] arr = {3.5, 7.1, 22.9, 2.3, 78.5};
+double[] arr = { 3.5, 7.1, 22.9, 2.3, 78.5 };
 PrintArray(arr);
 Console.WriteLine("");
-double[] diff = Diff(arr);
-PrintArray(diff);
-Console.WriteLine($"{diff[0]}");
+double diff = Diff(arr);
+Console.WriteLine($"{diff}");
 
-double[] Diff(double[] array)
+double Diff(double[] array)
 {
     double max = 0;
-    double min = 0;
-    double difference = max - min;
+    double min = arr[0];
     for (int i = 0; i < array.Length; i++)
     {
-        if(array[i] > max) max = array[i];
-        else if(array[i] < min) min = array[i];
+        if (array[i] > max)
+        {
+            max = array[i];
+        }
+        else if (array[i] < min)
+        {
+            min = array[i];
+        }
     }
-    
-    return new double[]{difference};
-    
+    double difference = max - min;
+    return difference;
+
 }
 
 void PrintArray(double[] arr)
@@ -31,7 +35,7 @@ void PrintArray(double[] arr)
     Console.Write("[");
     for (int i = 0; i < arr.Length; i++)
     {
-        if(i < arr.Length - 1)Console.Write($"{arr[i]}, ");
+        if (i < arr.Length - 1) Console.Write($"{arr[i]}, ");
         else Console.Write($"{arr[i]}");
     }
     Console.Write("]");
