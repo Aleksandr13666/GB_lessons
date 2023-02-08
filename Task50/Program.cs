a8e28bdd553fef1bd5f7d3052ca8c4922099f4f2
Aleksandr Ad <aleksandr-ad@narod.ru>
2023-02-08 22:42:21 +0700

    uncomplete Task 50

--- Task50/Program.cs
+++ Task50/Program.cs
@@ -12,3 +12,60 @@
 
 // 1, 7 -> такого элемента в массиве нет
 
+Console.WriteLine("Input X position of element: ");
+int xPos = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input Y position of element: ");
+int yPos = Convert.ToInt32(Console.ReadLine());
+
+int[,] CreateMatrix(int rows, int columns, int min, int max)
+{
+    int[,] matr = new int[rows, columns]; // 0, 1
+    Random rnd = new Random();
+
+    // for (int i = 0; i < rows; i++)
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = rnd.Next(min, max + 1);
+        }
+
+    }
+    return matr;
+}
+
+void MyFind(int[,] matr)
+{
+
+      for (int i = 0; i < matr.GetLength(0); i++)
+      {
+       for (int j = 0; j < matr.GetLength(1); i++)
+       {
+          if(matr[i, j] == matr[xPos, yPos])
+          {
+            Console.WriteLine($"Number of position is: {matr[i, j]}");
+          }
+          else Console.WriteLine("Out of index.");
+       } 
+      }
+    //   return new int[,]{};
+}
+
+void PrintMatr(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        Console.Write("[");
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            if (j < matr.GetLength(1) -1) Console.Write($"{matr[i, j],4} | ");
+            else Console.Write($"{matr[i, j],4} ");
+        }
+        Console.WriteLine("]");
+    }
+}
+
+int[,] myMatr = CreateMatrix(8, 8, 1, 9);
+PrintMatr(myMatr);
+MyFind(myMatr);
+// PrintMatr(myMatr);
