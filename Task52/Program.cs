ec2486a927c1545105b943df67e978dad1632dcb
Aleksandr Ad <aleksandr-ad@narod.ru>
2023-02-09 09:13:54 +0700

    Complete Task52

--- Task52/Program.cs
+++ Task52/Program.cs
@@ -7,5 +7,48 @@
 // Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
 
 
+int[,] CreateMatrix(int rows, int columns, int min, int max)
+{
+    int[,] matr = new int[rows,columns];
+    Random rnd = new Random();
 
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i,j] = rnd.Next(min, max);
+        }
+    }
+    return matr;
+}
 
+void ArithMean(int[,] matr)
+{
+    double arithmetic = 0;
+    for (int j = 0; j < matr.GetLength(1); j++)
+    {
+      for (int i = 0; i < matr.GetLength(0); i++)
+      {
+        arithmetic += matr[i, j]; 
+      }   
+      Console.WriteLine($"Arithmetic mean is {arithmetic / matr.GetLength(0)}");
+    }
+}
+
+void PrintMatr(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        Console.Write("[");
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+         if(j < matr.GetLength(1)-1) Console.Write($"{matr[i, j], 4} | ");
+         else Console.Write($"{matr[i, j],4}");   
+        }
+        Console.WriteLine("]");
+    }
+}
+
+int[,] matrix = CreateMatrix(4, 5, 1, 9);
+PrintMatr(matrix);
+ArithMean(matrix);
