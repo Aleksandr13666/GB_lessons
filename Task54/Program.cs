61c65a814100849d3f72d9402383a8933f787e86
Aleksandr Ad <aleksandr-ad@narod.ru>
2023-02-16 13:05:18 +0700

    task54

--- Task54/Program.cs
+++ Task54/Program.cs
@@ -6,4 +6,50 @@
 // В итоге получается вот такой массив:
 // 7 4 2 1
 // 9 5 3 2
-// // 8 4 4 2
+// 8 4 4 2
+
+
+int[,] createMatrix = CreateMatrix(5, 5, 1, 9);
+PrintMatr(createMatrix);
+
+
+int[,] CreateMatrix(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatr(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+        
+    {
+        Console.Write("[");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],4} |");
+            else Console.Write($"{matrix[i, j], 4} ");
+        }
+        Console.WriteLine("]");
+    }
+}
+
+void SortMatrix(int[,] matrix)
+{
+    int minimum = matrix[0, 0];
+
+    for (int i = 0; i < y / 2; i++)
+    {
+         temp = matrix[x, y];
+    }
+    
+
+}
