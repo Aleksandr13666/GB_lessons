bc6140e7198d5884a00675dc85e25071d6c491e8
Aleksandr Ad <aleksandr-ad@narod.ru>
2023-02-17 00:09:09 +0700

    Don't correctly work Task 56

--- Task56/Program.cs
+++ Task56/Program.cs
@@ -11,3 +11,68 @@
 // 5 2 6 7
 
 // Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
+
+
+
+
+
+int[,] CreateMatrix(int rows, int columns, int min, int max)
+{
+    int[,] matr = new int[rows, columns]; // 0, 1
+    Random rnd = new Random();
+
+
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = rnd.Next(min, max + 1);
+        }
+
+    }
+    return matr;
+}
+
+void PrintMatr(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        Console.Write("[");
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            if (j < matr.GetLength(1) - 1) Console.Write($"{matr[i, j],4} | ");
+            else Console.Write($"{matr[i, j],4} ");
+        }
+        Console.WriteLine("]");
+    }
+}
+
+void RowSumm(int[,] matr)
+{
+    int summ = 0;
+    int minimum = 0;
+    int index = 0;
+
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        summ = 0;
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            summ += matr[i, j];
+        }
+        Console.WriteLine($"summ of the row {i + 1} = {summ}");
+        if (summ < minimum)
+        {
+            minimum = summ;
+            index = i;
+        }
+
+    }
+
+    Console.WriteLine($"Minimal summ in row number {index}");
+}
+
+
+int[,] array2D = CreateMatrix(5, 6, 1, 15);
+PrintMatr(array2D);
+RowSumm(array2D);
