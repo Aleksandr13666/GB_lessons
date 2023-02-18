15d1d945cec10f786080124d5a65782b0dc643c0
Aleksandr Ad <aleksandr-ad@narod.ru>
2023-02-18 17:46:57 +0700

    added task67

---
+++ Task67/Program.cs
@@ -0,0 +1,21 @@
+﻿// 67. Напишите программу которая на вход будет принимать число
+// и возвращать сумму его чисел.
+// 123 - 6
+
+
+
+Console.WriteLine("Input a number ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int summ = Summ(num);
+Console.WriteLine($"{summ}");
+
+
+
+int Summ(int num)
+{
+    if (num == 0) return 0;
+    else return num % 10 + Summ (num / 10);
+}
+
+
